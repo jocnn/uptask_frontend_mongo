81a3da8c3375552dcd92a1f63271aac4fe3db386
jocnn <ijann_@outlook.com>
2022-05-28 16:16:15 -0300

    diseño de formulario recuperar contraseña

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,11 +1,50 @@
-import { Outlet } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const ForgetPassword = () => {
   return (
     <>
-      <h1>Recuperar Contraseña</h1>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Recupera tu Acceso y no pierdas tus {' '}<span className="text-slate-700">Proyectos</span>
+      </h1>
 
-      <Outlet />
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label 
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Correo Electrónico
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Correo Electrónico"
+            className="w-full mt-3 p-3 border rounded bg-gray-50"
+          />
+        </div>
+
+        <input 
+          type="submit" 
+          value="Enviar Instrucciones"
+          className="mb-5
+            bg-sky-700 w-full py-3 text-white 
+            uppercase font-bold rounded hover:cursor-pointer
+            hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >¿Ya tienes una cuenta? Iniciar Sesión
+        </Link>
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/registrar"
+        >¿No tienes una cuenta? Regístrate
+        </Link>
+      </nav>
     </>
   )
 }
