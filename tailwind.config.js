9c21105f8ca20585022f86f009f05319143020b6
jocnn <ijann_@outlook.com>
2022-05-28 09:46:27 -0300

    configurando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './components/**/*.{html,js,jsx}',
+    './pages/**/*.{html,js,jsx}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
