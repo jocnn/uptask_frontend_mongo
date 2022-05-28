5661750f8e29292c0bd9898ae4b0848da5bbf331
jocnn <ijann_@outlook.com>
2022-05-28 20:26:44 -0300

    diseño de pantalla para confirmación de cuenta

--- src/pages/ConfirmAccount.jsx
+++ src/pages/ConfirmAccount.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom"
 const ConfirmAccount = () => {
   return (
     <>
-      <h1>Confirmar cuenta</h1>
-
-      <Outlet />
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirma tu Cuenta y comienza a crear tus {' '}<span className="text-slate-700">Proyectos</span>
+      </h1>
     </>
   )
 }
