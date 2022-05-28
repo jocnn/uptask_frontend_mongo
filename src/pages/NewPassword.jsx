a41cd747655eda67aa57d45461d403624e1ead27
jocnn <ijann_@outlook.com>
2022-05-28 20:23:09 -0300

    diseño formulario nueva contraseña

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,11 +1,38 @@
-import { Outlet } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const NewPassword = () => {
   return (
     <>
-      <h1>Nuevo Password</h1>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Restablece tu password y no pierdas acceso a tus {' '}<span className="text-slate-700">Proyectos</span>
+      </h1>
 
-      <Outlet />
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+
+        <div className="my-5">
+          <label 
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Nueva Contraseña
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Escribe tu nueva contraseña"
+            className="w-full mt-3 p-3 border rounded bg-gray-50"
+          />
+        </div>
+
+        <input 
+          type="submit" 
+          value="Guardar Nueva Contraseña"
+          className="mb-5
+            bg-sky-700 w-full py-3 text-white 
+            uppercase font-bold rounded hover:cursor-pointer
+            hover:bg-sky-800 transition-colors"
+        />
+      </form>
     </>
   )
 }
