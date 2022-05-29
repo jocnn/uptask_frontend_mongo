b753245589eb2da4fe88411ee819b0fcb656f05c
jocnn <ijann_@outlook.com>
2022-05-29 12:21:25 -0300

    componente de alerta - exito o error

---
+++ src/components/Alert.jsx
@@ -0,0 +1,10 @@
+const Alert = ({ alerta }) => {
+  return (
+    <div className={` ${alerta.error ? 'from-red-400 to-red-600' : 'from-sky-400 to-sky-600'}
+      bg-gradient-to-br text-center p-3 rounded-xl uppercase text-white font-bold text-sm my-10`} >
+      <h1>{ alerta.msg }</h1>
+    </div>
+  )
+}
+
+export default Alert
