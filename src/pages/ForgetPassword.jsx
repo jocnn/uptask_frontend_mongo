b28d339bc72829d2cdabd3197f2868a096e452bd
jocnn <ijann_@outlook.com>
2022-05-30 20:12:52 -0300

    configurando contraseña y alerta

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import Alert from "../components/Alert"
 
 const ForgetPassword = () => {
+  
+  const [ email, setEmail ] = useState('')
+  const [ alerta, setAlerta ] = useState({})
+  
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if (email === '' || email.length < 6) {
+      setAlerta({
+        msg: 'El email es obligatorio',
+        error: true
+      })
+      return
+    }
+  }
+
+  const { msg } = alerta
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Recupera tu Acceso y no pierdas tus {' '}<span className="text-slate-700">Proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {
+        msg && 
+          <Alert 
+            alerta={alerta} 
+          />
+      }
+
+      <form 
+        onSubmit={handleSubmit}
+        className="my-10 bg-white shadow rounded-lg p-10"
+      >
         <div className="my-5">
           <label 
             htmlFor="email"
@@ -20,6 +50,8 @@ const ForgetPassword = () => {
             type="email"
             placeholder="Correo Electrónico"
             className="w-full mt-3 p-3 border rounded bg-gray-50"
+            value={email}
+            onChange={ e => setEmail(e.target.value) }
           />
         </div>
 
