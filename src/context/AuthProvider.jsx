62af428cd51d9c670c9702abeecfeb264576ce3d
jocnn <ijann_@outlook.com>
2022-05-31 10:35:36 -0300

    autenticando usando token

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,37 @@
 import { createContext , useState, useEffect} from 'react'
+import clienteAxios from '../config/clienteAxios'
 
 const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
 
   const [ auth, setAuth ] = useState({})
+
+  useEffect(() => {
+    const authUsuario = async () => {
+      const token = localStorage.getItem('token')
+
+      if (!token) {
+        return
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      try {
+        const { data } = await clienteAxios(`/usuarios/perfil`, config)
+        setAuth(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    authUsuario()
+  }, [])
 
   return (
     <AuthContext.Provider 
