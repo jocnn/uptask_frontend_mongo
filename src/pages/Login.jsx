82cbe1387ab934f1dc007e72ed00df974ae752c5
jocnn <ijann_@outlook.com>
2022-05-31 09:26:42 -0300

    autenticando usuario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,56 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import clienteAxios from '../config/clienteAxios'
+
+import Alert from '../components/Alert'
 
 const Login = () => {
+
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ alerta, setAlerta ] = useState({})
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if ([email, password].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      })
+      return
+    }
+
+    try {
+      const { data } = await clienteAxios.post(`/usuarios/login`, { email, password })
+      setAlerta({})
+      localStorage.setItem('token', data.token)
+      
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+
+  }
+
+  const { msg } = alerta
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Inicia Sesión y Administra tus {' '}<span className="text-slate-700">Proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      {
+        msg && <Alert alerta={alerta} />
+      }
+
+      <form 
+        onSubmit={handleSubmit}
+        className="my-10 bg-white shadow rounded-lg p-10"
+      >
         <div className="my-5">
           <label 
             htmlFor="email"
@@ -20,6 +63,8 @@ const Login = () => {
             type="email"
             placeholder="Email de registro"
             className="w-full mt-3 p-3 border rounded bg-gray-50"
+            value={email}
+            onChange={ e => setEmail(e.target.value) }
           />
         </div>
 
@@ -35,6 +80,8 @@ const Login = () => {
             type="password"
             placeholder="Password de registro"
             className="w-full mt-3 p-3 border rounded bg-gray-50"
+            value={password}
+            onChange={ e => setPassword(e.target.value) }
           />
         </div>
 
