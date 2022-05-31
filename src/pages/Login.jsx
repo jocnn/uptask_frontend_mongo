d947cc4712a75bef6ce4da9095271c182c683d09
jocnn <ijann_@outlook.com>
2022-05-31 09:45:25 -0300

    creando state para la autenticación

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import clienteAxios from '../config/clienteAxios'
+import useAuth from '../hooks/useAuth'
 
 import Alert from '../components/Alert'
 
@@ -9,6 +10,8 @@ const Login = () => {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ alerta, setAlerta ] = useState({})
+
+  const { setAuth } = useAuth()
 
   const handleSubmit = async e => {
     e.preventDefault()
@@ -25,7 +28,7 @@ const Login = () => {
       const { data } = await clienteAxios.post(`/usuarios/login`, { email, password })
       setAlerta({})
       localStorage.setItem('token', data.token)
-      
+      setAuth(data)
     } catch (error) {
       setAlerta({
         msg: error.response.data.msg,
