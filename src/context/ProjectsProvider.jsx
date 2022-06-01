9c376173411aa82e7a24dc226d39672882959dbf
jocnn <ijann_@outlook.com>
2022-06-01 10:16:13 -0300

    creando alerta y obtener proyecto de formulario

--- src/context/ProjectsProvider.jsx
+++ src/context/ProjectsProvider.jsx
@@ -6,11 +6,27 @@ const ProjectsContext = createContext()
 const ProjectsProvider = ({children}) => {
 
   const [ proyectos, setProyectos ] = useState([])
+  const [ alerta, setAlerta ] = useState({})
+
+  const mostrarAlerta = alerta => {
+    setAlerta(alerta)
+    
+    setTimeout(() => {
+      setAlerta({})
+    }, 3000);
+  }
+
+  const submitProyecto = async proyecto => {
+    console.log(proyecto)
+  }
 
   return (
     <ProjectsContext.Provider 
       value={{
         proyectos,
+        alerta,
+        mostrarAlerta,
+        submitProyecto
       }}
     >
       {children}
