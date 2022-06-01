4333c9d0dae95a0a9265c0b4c6f69fa069cf5309
jocnn <ijann_@outlook.com>
2022-06-01 08:54:47 -0300

    importando el provider en el BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,28 @@ import ConfirmAccount from "./pages/ConfirmAccount"
 import { AuthProvider } from "./context/AuthProvider"
 import Projects from "./pages/Projects"
 import NewProject from "./pages/NewProject"
+import { ProjectsProvider } from "./context/ProjectsProvider"
 
 function App() {
   
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="registrar" element={<Register />} />
-            <Route path="olvide-password" element={<ForgetPassword />} />
-            <Route path="olvide-password/:token" element={<NewPassword />} />
-            <Route path="confirmar/:id" element={<ConfirmAccount />} />
-          </Route>
-          <Route path="/proyectos" element={ <ProtectedRoute />}>
-            <Route index element={<Projects />} />
-            <Route path="crear-proyecto" element={<NewProject />} />
-          </Route>
-        </Routes>
+        <ProjectsProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Register />} />
+              <Route path="olvide-password" element={<ForgetPassword />} />
+              <Route path="olvide-password/:token" element={<NewPassword />} />
+              <Route path="confirmar/:id" element={<ConfirmAccount />} />
+            </Route>
+            <Route path="/proyectos" element={ <ProtectedRoute />}>
+              <Route index element={<Projects />} />
+              <Route path="crear-proyecto" element={<NewProject />} />
+            </Route>
+          </Routes>
+        </ProjectsProvider>
       </AuthProvider>
     </BrowserRouter>
   )
