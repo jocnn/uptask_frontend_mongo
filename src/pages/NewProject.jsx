4f37a3631a3d85f9303c2c887ee216fd1a9ef547
jocnn <ijann_@outlook.com>
2022-06-01 08:32:12 -0300

    creando página para nuevo proyecto

---
+++ src/pages/NewProject.jsx
@@ -0,0 +1,17 @@
+const NewProject = () => {
+  return (
+    <>
+      <h1
+        className="text-4xl font-black"
+      >
+        Nuevo Proyecto
+      </h1>
+
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default NewProject
