ecae037f7fd3b862cb6859ed57aadb7e0adb7a9e
jocnn <ijann_@outlook.com>
2022-06-02 09:30:19 -0300

    componente para la lista de proyectos

---
+++ src/components/PreviewProject.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+const PreviewProject = ({ proyecto }) => {
+
+  const { nombre, _id, cliente } = proyecto
+
+  return (
+    <div className="border-b p-5 flex">
+      <p className="flex-1">
+        {nombre}
+        <span
+          className="text-sm text-gray-500 uppercase"
+        >{' '}{cliente}</span>
+      </p>
+      <Link
+        className="text-gray-600 hover:text-gray-800 uppercase text-sm font-bold"
+        to={`${_id}`}
+      >Ver Proyecto</Link>
+    </div>
+  )
+}
+
+export default PreviewProject
